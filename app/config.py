8157def0097294cd614b0614ea32a83c7dc86ddf
lyzhenko <alexander.lyzhenko@yandex.ru>
2026-07-16 07:41:38 +0800

    init commit

---
+++ app/config.py
@@ -0,0 +1,22 @@
+from pydantic import computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Setting(BaseSettings):
+    DB_HOST: str 
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @computed_field
+    @property
+    def DATABASE_URL(self) -> str:   # только self, без cls и value
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8'
+    )
+
+settings  = Setting()
