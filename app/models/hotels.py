638aba0c6fd0710157ece233c69db3973bf8d000
lyzhenko <alexander.lyzhenko@yandex.ru>
2026-07-17 22:53:15 +0800

    Добавил документацию для моделей Bookings, Hotels, Rooms и Users

--- app/models/hotels.py
+++ app/models/hotels.py
@@ -3,6 +3,20 @@ from app.database import Base
 
 
 class Hotels(Base):
+    """
+    Модель для представления отелей в системе.
+    
+    Хранит информацию об отелях, включая название, местоположение, 
+    доступные услуги и количество номеров.
+    
+    Attributes:
+        id: Уникальный идентификатор отеля (первичный ключ).
+        name: Название отеля.
+        location: Местоположение/адрес отеля.
+        services: JSON поле со списком услуг, предоставляемых отелем.
+        rooms_quanyity: Количество номеров в отеле.
+        image_id: ID изображения отеля.
+    """
     __tablename__ = "hotels"
     id: Column = Column(Integer, primary_key=True)
     name: Column = Column(String, nullable=False)
