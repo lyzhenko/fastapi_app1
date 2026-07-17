bc9e3d17dde1e467af7ee38f2f58b232f1cf3efc
lyzhenko <alexander.lyzhenko@yandex.ru>
2026-07-17 20:35:28 +0800

    Добавил поддержку фильтрации в методы find_all, find_one_or_none и find_by_id

--- app/services/base.py
+++ app/services/base.py
@@ -9,11 +9,31 @@ class BaseService:
     model: ClassVar[Optional[Type[DeclarativeBase]]] = None
 
     @classmethod
-    async def find_all(cls):
+    async def find_all(cls, **kwargs):
         if cls.model is None:
             raise NotImplementedError("Model class must be set in subclass")
 
         async with async_session_maker() as session:
-            quuery = select(cls.model)
+            quuery = select(cls.model).filter_by(**kwargs)
             result = await session.execute(quuery)
             return result.scalars().all()
+
+    @classmethod
+    async def find_one_or_none(cls, **kwargs):
+        if cls.model is None:
+            raise NotImplementedError("Model class must be set in subclass")
+
+        async with async_session_maker() as session:
+            quuery = select(cls.model).filter_by(**kwargs)
+            result = await session.execute(quuery)
+            return result.scalar_one_or_none()
+
+    @classmethod
+    async def find_by_id(cls, model_id: int):
+        if cls.model is None:
+            raise NotImplementedError("Model class must be set in subclass")
+
+        async with async_session_maker() as session:
+            quuery = select(cls.model).filter_by(id=model_id)
+            result = await session.execute(quuery)
+            return result.scalar_one_or_none()
