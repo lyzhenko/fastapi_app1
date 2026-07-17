7f4e5849502c1920cf99b3959e39309233ed9bca
lyzhenko <alexander.lyzhenko@yandex.ru>
2026-07-17 22:49:57 +0800

    Добавил документацию для методов поиска в классе BaseService

--- app/services/base.py
+++ app/services/base.py
@@ -6,10 +6,39 @@ from app.database import async_session_maker
 
 
 class BaseService:
+    """
+    Базовый класс сервиса для выполнения CRUD операций с базой данных.
+    
+    Предоставляет асинхронные методы для поиска записей в базе данных.
+    Наследники должны установить переменную класса `model` на нужную модель SQLAlchemy.
+    
+    Attributes:
+        model: Модель SQLAlchemy, с которой работает сервис. Должна быть переопределена в подклассе.
+    
+    Example:
+        class HotelsService(BaseService):
+            model = Hotel
+        
+        hotels = await HotelsService.find_all(city="Moscow")
+        hotel = await HotelsService.find_by_id(1)
+    """
+
     model: ClassVar[Optional[Type[DeclarativeBase]]] = None
 
     @classmethod
     async def find_all(cls, **kwargs):
+        """
+        Получить все записи модели с опциональной фильтрацией.
+        
+        Args:
+            **kwargs: Параметры фильтрации (например, city="Moscow", status="active").
+        
+        Returns:
+            list: Список всех найденных записей, соответствующих фильтрам.
+        
+        Raises:
+            NotImplementedError: Если модель не установлена в подклассе.
+        """
         if cls.model is None:
             raise NotImplementedError("Model class must be set in subclass")
 
@@ -20,6 +49,18 @@ class BaseService:
 
     @classmethod
     async def find_one_or_none(cls, **kwargs):
+        """
+        Получить одну запись или None, если не найдена.
+        
+        Args:
+            **kwargs: Параметры фильтрации.
+        
+        Returns:
+            Найденная запись или None.
+        
+        Raises:
+            NotImplementedError: Если модель не установлена в подклассе.
+        """
         if cls.model is None:
             raise NotImplementedError("Model class must be set in subclass")
 
